f788c68da900cc56e6886f7b7540fe7c4e7df972
zach-laptop <zach.macdonald.021@gmail.com>
2022-06-25 18:30:55 -0400

    asdf

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,9 +1,21 @@
 <template>
+    <p>
+        This is a static HTML implementation of our project. In its current state, it is not functional. Many features
+        will be added as we submit more deliverables.
+    </p>
+    <p>
+        This is a tool to stay organized with uOttawa homework and keeping up to date on lecture recordings.
+    </p>
+    <p>
+        Keep track of your classes on the <router-link to="/classes">Classes</router-link> page.
+        <br>
+        And stay up to date on your home work on the <router-link to="/homework">Homework</router-link> page.
+    </p>
 </template>
 
 <script setup>
 
 </script>
 
-<style>
+<style scoped>
 </style>
