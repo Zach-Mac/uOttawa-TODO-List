dad0dad7d879ff55c4295e8b56fc800ed1a0ebf4
Zack <zackstempowicz@gmail.com>
2022-06-25 17:06:32 -0400

    homework

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <form>
+        <input style="margin-right: 10px;" type="text" placeholder="Please enter your assignment" name="enter-todo">
+        <input style="margin-right: 10px;" type="text" placeholder="Please enter your class" name="enter-class">
+        <input type="text" placeholder="Please enter the due date" name="enter-duedate">
+    </form>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+    
+</style>
