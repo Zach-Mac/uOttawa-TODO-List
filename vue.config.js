a37f83acc8c078247cfb815a521453e315e25181
zach-laptop <zach.macdonald.021@gmail.com>
2022-06-25 16:19:49 -0400

    asdf

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,32 @@
 module.exports = {
     pages: {
-        home: 'src/pages/home/main.js',
-        about: 'src/pages/about/main.js',
-        classes: 'src/pages/classes/main.js',
-        homework: 'src/pages/homework/main.js'
+        home: {
+            entry: 'src/pages/home/main.js',
+            template: 'public/index.html',
+            filename: 'home.html',
+            title: 'Home Page'
+        },
+        about: {
+            entry: 'src/pages/about/main.js',
+            template: 'public/index.html',
+            filename: 'about.html',
+            title: 'About Page'
+        },
+        homework: {
+            entry: 'src/pages/homework/main.js',
+            template: 'public/index.html',
+            filename: 'homework.html',
+            title: 'Homework Page'
+        },
+        classes: {
+            entry: 'src/pages/classes/main.js',
+            template: 'public/index.html',
+            filename: 'classes.html',
+            title: 'Classes Page'
+        }
+        // about: 'src/pages/about/main.js',
+        // classes: 'src/pages/classes/main.js', //schedule
+        // homework: 'src/pages/homework/main.js' //todo list
+
     }
 }
