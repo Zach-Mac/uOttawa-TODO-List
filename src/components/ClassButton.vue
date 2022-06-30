e3146c166dcbace8e9739b4e65fb4bd5f5acfc47
zach-laptop <zach.macdonald.021@gmail.com>
2022-06-30 18:34:51 -0400

    added functionality to classes page

---
+++ src/components/ClassButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <button
+    class="classButton"
+    :class="{ completed: completed }"
+    @click="completed = !completed"
+  >
+    <s v-if="completed">
+      {{ name }}
+    </s>
+    <b v-else>
+      {{ name }}
+    </b>
+  </button>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const completed = ref(props.completed);
+</script>
+
+<style scoped>
+/* make the class button background a bright blue with big bold
+ white text and a slim black border
+ also, make it fit the width of the parent table slot */
+.classButton {
+  width: 100%;
+  height: 100%;
+  background-color: blue;
+  color: white;
+  border: 1px solid black;
+  border-radius: 5px;
+}
+
+/* make the class button background red with black text when it is completed */
+.classButton.completed {
+  background-color: red;
+  color: black;
+}
+</style>
--- src/views/ClassesPage.vue
+++ src/views/ClassesPage.vue
@@ -1,140 +1,185 @@
 <template>
-    <div class="calendar">
-        <div class="column">
-            <form class="form">
-                <input placeholder="Enter Class" class="enterClass">
-                <input class="time" type="time" placeholder="Enter Time">
-                <select name="test" class="daySelector">
-                    <option v-for="day in days" :value="day">{{day}}</option>
-                </select>
-                <input class="addClass" type="submit" value="Add Class to Schedule">
-            </form>
-                
-            <table border = "1" class="table">
-                <tr>
-                    <th></th>
-                    <th v-for="day in days">{{day}}</th>
-                </tr>
-                <tr></tr>
+  <form @submit="addClass" class="form">
+    <input placeholder="Enter Class" class="enterClass" />
+    <!-- add label to selecttimeslot -->
+    <select class="selectTimeSlot">
+      <option value="">Select Timeslot</option>
+      <option v-for="timeSlot in timeSlots.length" :value="index">
+        {{ timeSlot }}
+      </option>
+    </select>
+    <!-- <select class="selectTimeSlot">
+      <option v-for="timeSlot in timeSlots.length" :value="timeSlot">
+        {{ timeSlot }}
+      </option>
+    </select> -->
+    <select name="test" class="daySelector">
+      <option v-for="day in days" :value="day">{{ day }}</option>
+    </select>
+    <button class="addClassButton" type="submit">Add Class</button>
+    <!-- <button class="addClass" type="submit" value="Add Class to Schedule" /> -->
+  </form>
 
-                    <tr v-for="time in numTimes">
-                        <td>{{ toTime(time + timeStart - 1) }}</td>
-                        <td v-for="day in days"></td>
-                    </tr>
+  <table border="1" class="table">
+    <tr>
+      <th></th>
+      <th v-for="day in days">{{ day }}</th>
+    </tr>
+    <tr></tr>
 
-            </table>
-        </div>
-        
-        <div class="column">
-            <img src="@/assets/thanos.gif" alt="">
-        </div>
+    <tr v-for="(time, index) in timeSlots">
+      <td>{{ toTime(time) }}</td>
 
-    </div>
+      <td v-for="day in days">
+        <ClassButton
+          v-if="classes[day][index]"
+          :name="classes[day][index].name"
+          :completed="classes[day][index].completed"
+        />
+      </td>
+    </tr>
+  </table>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import ClassButton from "../components/ClassButton.vue";
 const days = [
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-    'Sunday'
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
 ];
 
 const timeStart = 8;
-const numTimes = 12;
+const timeEnd = 21;
 
-function toTime(time) {
-    time = time % 12 + ":00 " + (parseInt(time / 12) % 2 == 0 ? "AM" : "PM");
-    if (time == "0:00 AM") {
-        time = "12:00 AM";
-    } else if (time == "0:00 PM") {
-        time = "12:00 PM";
-    }
-    return time;
+let timeSlots = [];
+for (let i = timeStart; i <= timeEnd; i += 1.5) {
+  timeSlots.push(i);
 }
+
+// toTime converts a time slot number to a string
+function toTime(timeSlot) {
+  const hour = Math.floor(timeSlot);
+  const minute = Math.round((timeSlot - hour) * 60);
+  const ampm = hour < 12 ? "AM" : "PM";
+
+  // convert hour to 12-hour format and add trailing 0 to minutes if necessary
+  const hour12 = hour % 12 || 12;
+  const minuteString = minute < 10 ? `0${minute}` : minute;
+  return `${hour12}:${minuteString} ${ampm}`;
+}
+
+const classes = ref([]);
+days.forEach((day) => {
+  classes.value[day] = [];
+  //   for (let i = 0; i < numTimes; i++) {
+  //     classes.value[day][i] = null;
+  //   }
+});
+
+// addClass adds a class to the schedule
+function addClass(e) {
+  e.preventDefault();
+  const form = e.target;
+  const name = form.querySelector(".enterClass").value;
+  const timeSlot = form.querySelector(".selectTimeSlot").value;
+  const day = form.querySelector(".daySelector").value;
+  classes.value[day][timeSlot - 1] = {
+    name,
+    completed: false,
+  };
+  console.log("classes", classes.value);
+}
+
+classes.value["Monday"][2] = {
+  name: "Math",
+  completed: false,
+};
+
+console.log("classes", classes.value);
 </script>
 
 <style scoped>
-
 /* column classes are side by side */
 .column {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 50%;
-    height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+  height: 100%;
 }
 
 /* elements inside calendar classes are all on the same line.
 if the display width is less than 1200px, the calendar will be in a row */
 .calendar {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
 }
 
-@media (max-width: 1200px) {
-    .calendar {
-        flex-direction: column;
-    }
-}
-/* style addClass. It should be thin, and inline with the rest of the inputs. Also make it green. */
-.addClass {
-    width: 150px;
-    height: 30px;
-    background-color: green;
-    color: white;
-    border: none;
-    border-radius: 5px;
-    margin-left: 10px;
+/* style submit button. It should be thin, and inline with the rest of the inputs. Also make it green. */
+.addClassButton {
+  display: inline-block;
+  width: 100px;
+  height: 30px;
+  background-color: green;
+  color: white;
+  border: 1px solid black;
+  border-radius: 5px;
 }
 
-/* style time */
-.time {
-    width: 150px;
-    height: 30px;
-    border: 1;
-    border-radius: 5px;
-    margin-left: 10px;
+/* style selecttimeslot */
+.selectTimeSlot {
+  width: 150px;
+  height: 30px;
+  color: black;
+  border: 1;
+  border-radius: 5px;
+  margin-left: 10px;
 }
 
 /* style daySelector. give it a border */
 .daySelector {
-    width: 150px;
-    height: 30px;
-    border: 1;
-    border-radius: 5px;
-    margin-left: 10px;
+  width: 150px;
+  height: 30px;
+  border: 1;
+  border-radius: 5px;
+  margin-left: 10px;
 }
 
 .enterClass {
-    width: 150px;
-    height: 30px;
-    border: 1;
-    border-radius: 5px;
-    margin-left: 10px;
+  width: 150px;
+  height: 30px;
+  border: 1;
+  border-radius: 5px;
+  margin-left: 10px;
 }
 
 /* form class shoud line up */
 .form {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-}
-/* make all entries in table the same width */
-table {
-  table-layout: fixed ;
-  width: 100% ;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
 }
 
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+  width: 100%;
+  height: 100%;
+  min-width: 100%;
+  min-height: 100%;
+}
 </style>
